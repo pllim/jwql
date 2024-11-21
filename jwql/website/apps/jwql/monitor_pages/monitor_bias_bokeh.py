3435309cd64dcde90334abe56d36498c6dcca3d3
Ben Sunnquist <bsunnquist@stsci.edu>
2024-11-21 16:05:00 -0500

    remove duplicate datetime import

--- jwql/website/apps/jwql/monitor_pages/monitor_bias_bokeh.py
+++ jwql/website/apps/jwql/monitor_pages/monitor_bias_bokeh.py
@@ -29,7 +29,6 @@ from bokeh.models import ColumnDataSource, DatetimeTickFormatter, HoverTool
 from bokeh.models.layouts import Tabs, TabPanel
 from bokeh.plotting import figure, output_file, save
 from bokeh.resources import CDN
-from datetime import datetime
 import numpy as np
 import pandas as pd
 
